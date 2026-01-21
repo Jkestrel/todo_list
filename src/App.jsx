6ab59978db9a86161b2ef910359ed8b869abcf26
Christo <kestreljack7@gmail.com>
2026-01-21 23:31:34 +0530

    All files added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import ToDoList from './components/ToDoList'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+    <Header></Header>
+    <ToDoList></ToDoList>
+    </>
+  )
+}
+
+export default App
